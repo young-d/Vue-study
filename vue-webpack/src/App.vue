1031199daa06e8b3631663b0d0aed5dac1399111
young-d <dodoga096@gmail.com>
2021-10-04 11:24:31 +0900

    vue sfc with webpack

---
+++ vue-webpack/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 v-if="msg" class="message" @click="log">{{ msg }}</h1>
+    <Hello />
+</template>
+
+<script>
+import Hello from '~/components/Hello';
+
+export default {
+    components: {
+        Hello
+    },
+    data() {
+        return {
+            msg: 'Hello Vue SFC dev server!!!'
+        }
+    }
+}
+</script>
